76feba67a162a52b59b10332fc5aadf3d1013e81
Kaedenn <kaedenn@gmail.com>
2024-07-24 19:22:02 -0800

    Slight changes, add GTC resolving

--- files/utility/treasure_chest.lua
+++ files/utility/treasure_chest.lua
@@ -1,5 +1,20 @@
 --[[ Treasure chest content prediction ]]
 
+--[[ FIXME: This doesn't seem to work for greater treasure chests.
+-- See https://kaliuresis.github.io/noa/ and invoke the following for testing:
+cx, cy = 0, 0               -- Update with values from NOA
+SetRandomSeed(cx, cy))
+print_rewards(do_chest_get_rewards_super(cx, cy, 0, cx, cy, false))
+
+print_rewards(chest_get_rewards(entity_id))
+
+dofile("mods/world_radar/files/utility/treasure_chest.lua")
+cpos = {{-293, -2945}, {-61, 261}}
+cx, cy = unpack(cpos[2])
+SetRandomSeed(cx, cy)
+print_rewards(do_chest_get_rewards_super(cx, cy, 0, cx, cy, false))
+--]]
+
 dofile_once("data/scripts/lib/utilities.lua")
 dofile_once("data/scripts/gun/gun_actions.lua")
 dofile_once("data/scripts/game_helpers.lua")
@@ -19,12 +34,19 @@ function chest_get_rewards(entity_id)
         rand_y = tonumber(ComponentGetValue(position_comp, "pos_y"))
     end
 
-    -- Offsets taken from data/scripts/items/chest_random.lua
-    rand_x = rand_x + 509.7
-    rand_y = rand_y + 683.1
+    local fname = EntityGetFilename(entity_id)
+    local rewards
+    if fname:match("chest_random_super") then
+        SetRandomSeed(rand_x, rand_y)
+        rewards = do_chest_get_rewards_super(x, y, entity_id, rand_x, rand_y, false)
+    else
+        -- Offsets taken from data/scripts/items/chest_random.lua
+        rand_x = rand_x + 509.7
+        rand_y = rand_y + 683.1
 
-    SetRandomSeed(rand_x, rand_y)
-    local rewards = do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, false)
+        SetRandomSeed(rand_x, rand_y)
+        rewards = do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, false)
+    end
 
     return rewards
 end
@@ -49,7 +71,7 @@ function do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, set_rand_)
             reward.name = "$action_bomb"
             reward.entity = "data/entities/projectiles/bomb_small.xml"
         elseif rnd <= 40 then -- Gold
-            reward.type = "item"
+            reward.type = "gold"
             reward.name = "$item_goldnugget"
             reward.amount = 0
 
@@ -77,10 +99,14 @@ function do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, set_rand_)
                 if Random(0, 100) > 50 then
                     tamount = Random(1, 3)
                     reward.amount = reward.amount + tamount * 200
+                    dummy = Random(-10, 10) -- Discard for x position
+                    dummy = Random(-10, 5) -- Discard for y position
                 end
                 if Random(0, 100) > 80 then
                     tamount = Random(1, 3)
-                    reward.amount = reward.amount + tamount * 200
+                    reward.amount = reward.amount + tamount * 1000
+                    dummy = Random(-10, 10) -- Discard for x position
+                    dummy = Random(-10, 5) -- Discard for y position
                 end
             end
         elseif rnd <= 50 then -- Potion
@@ -119,6 +145,7 @@ function do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, set_rand_)
                 reward.entity = "data/entities/animals/illusions/shaman_wind.xml"
             end
         elseif rnd <= 60 then -- Misc items
+            local greeding = GameHasFlagRun("greed_curse") and not GameHasFlagRun("greed_curse_gone")
             local opts = {
                 "data/entities/items/pickup/safe_haven.xml",
                 "data/entities/items/pickup/moon.xml",
@@ -135,7 +162,7 @@ function do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, set_rand_)
                 local flag_status = HasFlagPersistent("card_unlocked_duplicate")
                 if flag_status then
                     reward.type = "item"
-                    if GameHasFlagRun("greed_curse") and not GameHasFlagRun("greed_curse_gone") then
+                    if greeding then
                         reward.name = "$item_greed_die"
                         reward.entity = "data/entities/items/pickup/physics_greed_die.xml"
                     else
@@ -158,7 +185,7 @@ function do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, set_rand_)
                 reward.type = "item"
                 reward.name = "$item_gold_orb"
                 reward.entity = "data/entities/items/pickup/physics_gold_orb.xml"
-                if GameHasFlagRun("greed_curse") and not GameHasFlagRun("greed_curse_gone") then
+                if greeding then
                     reward.name = "$item_gold_orb_greed"
                     reward.entity = "data/entities/items/pickup/physics_gold_orb_greed.xml"
                 end
@@ -194,7 +221,7 @@ function do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, set_rand_)
                 end
             end
         elseif rnd <= 84 then -- Wand
-            rnd = Random(0,100)
+            rnd = Random(0, 100)
             reward.type = "wand"
             if rnd <= 25 then
                 reward.name = "$item_wand (level 1)"
@@ -239,8 +266,8 @@ function do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, set_rand_)
                 reward.entity = "data/entities/items/pickup/heart_fullhp.xml"
             end
         elseif rnd <= 98 then -- Converts the chest to gold
-            reward.type = "gold"
-            reward.name = "convert to $mat_gold"
+            reward.type = "convert"
+            reward.name = "$mat_gold"
         elseif rnd <= 99 then -- Add 2 to the reward count
             count = count + 2
             reward.type = "reroll"
@@ -250,6 +277,112 @@ function do_chest_get_rewards(x, y, entity_id, rand_x, rand_y, set_rand_)
             count = count + 3
             reward.type = "reroll"
             reward.name = "reroll"
+            reward.amount = 3
+        end
+
+        if reward.type ~= nil then
+            table.insert(rewards, reward)
+        end
+    end
+
+    return rewards
+end
+
+--[[ Obtain the greater treasure chest rewards for the given entity ]]
+function do_chest_get_rewards_super(x, y, entity_id, rand_x, rand_y, set_rand_)
+    local set_rand = false
+    if set_rand_ ~= nil then set_rand = set_rand_ end
+    if set_rand then
+        SetRandomSeed(GameGetFrameNum(), x + y + entity_id)
+    end
+
+    if Random(0, 100000) >= 100000 then
+        return {{type="sampo"}}
+    end
+
+    local rewards = {}
+    local count = 1
+    while count > 0 do
+        count = count - 1
+        local rnd = Random(1, 100)
+
+        local reward = {type=nil, name=nil, entity=nil, amount=nil}
+        if rnd <= 10 then
+            rnd = Random(0, 100)
+            reward.type = "potions"
+            reward.name = "$item_potion"
+            reward.entities = {}
+            if rnd <= 30 then
+                table.insert(reward.entities, "data/entities/items/pickup/potion.xml")
+                table.insert(reward.entities, "data/entities/items/pickup/potion.xml")
+                table.insert(reward.entities, "data/entities/items/pickup/potion_secret.xml")
+            else
+                table.insert(reward.entities, "data/entities/items/pickup/potion_secret.xml")
+                table.insert(reward.entities, "data/entities/items/pickup/potion_secret.xml")
+                table.insert(reward.entities, "data/entities/items/pickup/potion_random_material.xml")
+            end
+        elseif rnd <= 15 then
+            reward.type = "goldrain"
+            reward.name = "gold rain"
+            reward.entity = "data/entities/projectiles/rain_gold.xml"
+        elseif rnd <= 18 then
+            rnd = Random(1, 30)
+            reward.type = "item"
+            if rnd ~= 30 then
+                reward.name = "$item_waterstone"
+                reward.entity = "data/entities/items/pickup/waterstone.xml"
+            else
+                reward.name = "$item_poopstone"
+                reward.entity = "data/entities/items/pickup/poopstone.xml"
+            end
+        elseif rnd <= 39 then
+            rnd = Random(0, 100)
+            reward.type = "wand"
+            if rnd <= 25 then
+                reward.name = "$item_wand (level 4)"
+                reward.entity = "data/entities/items/wand_level_04.xml"
+            elseif rnd <= 50 then
+                reward.name = "$item_wand (level 4) (unshuffle)"
+                reward.entity = "data/entities/items/wand_unshuffle_04.xml"
+            elseif rnd <= 75 then
+                reward.name = "$item_wand (level 5)"
+                reward.entity = "data/entities/items/wand_level_05.xml"
+            elseif rnd <= 90 then
+                reward.name = "$item_wand (level 5) (unshuffle)"
+                reward.entity = "data/entities/items/wand_unshuffle_05.xml"
+            elseif rnd <= 96 then
+                reward.name = "$item_wand (level 6)"
+                reward.entity = "data/entities/items/wand_level_06.xml"
+            elseif rnd <= 98 then
+                reward.name = "$item_wand (level 6) (unshuffle)"
+                reward.entity = "data/entities/items/wand_unshuffle_06.xml"
+            elseif rnd <= 99 then
+                reward.name = "$item_wand (level 6)"
+                reward.entity = "data/entities/items/wand_level_06.xml"
+            elseif rnd <= 100 then
+                reward.name = "$item_wand (level 10)"
+                reward.entity = "data/entities/items/wand_level_10.xml"
+            end
+        elseif rnd <= 60 then
+            rnd = Random(0, 100)
+            reward.type = "entity"
+            if rnd <= 89 then
+                reward.name = "$item_heart"
+                reward.entity = "data/entities/items/pickup/heart.xml"
+            elseif rnd <= 99 then
+                reward.name = "$item_heart_better"
+                reward.entity = "data/entities/items/pickup/heart_better.xml"
+            else
+                reward.name = "$item_heart_fullhp"
+                reward.entity = "data/entities/items/pickup/heart_fullhp.xml"
+            end
+        elseif rnd <= 98 then
+            count = count + 2
+            reward.type = "reroll"
+            reward.amount = 2
+        elseif rnd <= 100 then
+            count = count + 3
+            reward.type = "reroll"
             reward.amount = 3
         end
 
@@ -293,5 +426,47 @@ function get_random_card(card_x, card_y)
     return nil
 end
 
--- vim: set ts=4 sts=4 sw=4:
+--[[ Generate text for use in Panel:p ]]
+function format_rewards(rewards)
+    local text = {}
+    for idx, reward in ipairs(rewards) do
+        if reward.type == "wand" then
+            table.insert(text, ("Wand: %s [%s]"):format(reward.name, reward.entity))
+        elseif reward.type == "card" then
+            for sidx, spell in ipairs(reward.entities) do
+                table.insert(text, ("Spell: %s"):format(spell))
+            end
+        elseif reward.type == "gold" then
+            table.insert(text, ("%d %s"):format(reward.amount, reward.name))
+        elseif reward.type == "convert" then
+            table.insert(text, ("Convert entity to %s"):format(reward.name))
+        elseif reward.type == "item" then
+            table.insert(text, ("Item: %s [%s]"):format(reward.name, reward.entity))
+        elseif reward.type == "entity" then
+            table.insert(text, ("Entity: %s [%s]"):format(reward.name, reward.entity))
+        elseif reward.type == "potion" then
+            table.insert(text, ("Potion: %s [%s]"):format(reward.name, reward.entity))
+        elseif reward.type == "pouch" then
+            table.insert(text, ("Pouch: %s [%s]"):format(reward.name, reward.entity))
+        elseif reward.type == "reroll" then
+            table.insert(text, ("Reroll %dx"):format(reward.amount))
+        elseif reward.type == "potions" then
+            table.insert(text, ("Multiple potions: %s"):format(table.concat(reward.entities, ", ")))
+        elseif reward.type == "goldrain" then
+            table.insert(text, "Gold rain")
+        else
+            table.insert(text, ("Invalid reward %s"):format(reward.type))
+        end
+    end
+    return text
+end
 
+--[[ Print the rewards via the given imgui object (TODO) ]]
+function print_rewards(rewards, imgui)
+    local text = format_rewards(rewards)
+    for _, reward in ipairs(text) do
+        GamePrint(reward)
+    end
+end
+
+-- vim: set ts=4 sts=4 sw=4:
